3867f09b4031f46ad9c96abf8307a605871e2c62
afolabiopakunle <opakunleafolabi@gmail.com>
2021-11-12 18:24:14 +0100

    form works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component, VERSION } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   form: FormGroup;
 
-  
+  constructor(private fb: FormBuilder) {}
+  ngOnInit() {
+    this.form = this.fb.group({
+      fullName: this.fb.control(null, [Validators.required]),
+      email: this.fb.control(null, [Validators.required]),
+      skillSummary: this.fb.control(null),
+      skills: this.fb.group({
+        skillLevel: this.fb.control(null),
+        department: this.fb.control(null)
+      })
+    });
+  }
+
+  check() {
+    console.log(this.form.value);
+  }
 }
