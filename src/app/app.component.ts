b1508ec6c8a75217b5ae72bd5e1f0ac0a638dbf2
afolabiopakunle <opakunleafolabi@gmail.com>
2021-11-12 17:58:06 +0100

    angular-form added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,7 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  form: FormGroup;
+
+  
 }
